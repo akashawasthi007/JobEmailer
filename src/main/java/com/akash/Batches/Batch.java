58a9b250aef3ed0440c0ea77465e05bfb035e56f
Akash Awasthi <akashawasthi076@gmail.com>
2025-03-03 02:35:26 +0530

    solving autoiring issue

---
+++ src/main/java/com/akash/Batches/Batch.java
@@ -0,0 +1,12 @@
+package com.akash.Batches;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Batch {
+
+    public String run() {
+       return new String("Test");
+    }
+
+}
--- src/main/java/com/akash/emailer/EmailerApplication.java
+++ src/main/java/com/akash/emailer/EmailerApplication.java
@@ -2,12 +2,15 @@ package com.akash.emailer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.akash.controller")
 public class EmailerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailerApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
